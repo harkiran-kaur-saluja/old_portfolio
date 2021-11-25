60c7fae83b5cf4deae94abe2c64fd7ac69cb25ec
harkisa9 <harkisa9@uw.edu>
2021-11-25 03:15:08 -0500

    complete general setup

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, {useState} from 'react';
+import {Route, Switch, Redirect} from 'react-router-dom';
+import "./css/style.css";
+
+function App(props) {
+    return (<div></div>);
+}
+
+export default App;
